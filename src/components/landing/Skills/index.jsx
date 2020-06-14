2e5090759ae38d6cd0c4766541d59bd092fefcee
Ashraful Islam <ashrafulrobin3@gmail.com>
2020-06-14 20:10:15 +0600

    Updated site data according my portfolio

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -8,13 +8,17 @@ export const Skills = () => (
   <Wrapper id="about">
     <SkillsWrapper as={Container}>
       <Thumbnail>
-        <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+        <img src={dev} alt="I’m Ashraful and I’m a Backend & DevOps Engineer!" />
       </Thumbnail>
       <Details>
         <h1>Hi There!</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+          I have following skills,
+          <p>
+            <code>Python</code>, <code>Django</code>, <code>Flask</code>, <code>REST API</code>, <code>GraphQL</code>
+            <br />
+            <code>JavaScript</code>, <code>NodeJS</code>, <code>VueJS</code>, <code>React</code>
+          </p>
         </p>
         <Button as={AnchorLink} href="#contact">
           Hire me
--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -7,13 +7,9 @@ export const Footer = () => (
   <Wrapper>
     <Flex as={Container}>
       <Details>
-        <h2>John Doe</h2>
+        <h2>Mohammad Ashraful Islam</h2>
         <span>
-          © All rights are reserved | {new Date().getFullYear()} | Made with{' '}
-          <span aria-label="love" role="img">
-            💖
-          </span>{' '}
-          by{' '}
+          © All rights are reserved | {new Date().getFullYear()} | Thanks to{' '}
           <a href="https://smakosh.com/?ref=portfolio-dev" rel="noopener noreferrer" target="_blank">
             Smakosh
           </a>
