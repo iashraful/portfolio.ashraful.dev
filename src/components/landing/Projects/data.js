3c1801c0225b32871aafb2fdfa4746c211a1de38
Ashraful Islam <ashrafulrobin3@gmail.com>
2020-08-14 12:47:55 +0600

    Added dev.to blog

--- src/components/landing/Projects/data.js
+++ src/components/landing/Projects/data.js
@@ -6,4 +6,11 @@ export default [
     website: 'https://baraticn.com',
     tags: ['Python', 'Django', 'REST API', 'VueJS', 'Vuetify', 'PWA'],
   },
+  {
+    name: 'Dev.to API client',
+    description: "It's a https://dev.to API based client for blogging. It's a PWA",
+    cover: 'https://thepracticaldev.s3.amazonaws.com/i/6hqmcjaxbgbon8ydw93z.png',
+    website: 'http://theashraful.xyz:5000/',
+    tags: ['Dev.to', 'REST API', 'VueJS', 'PWA'],
+  },
 ];
